4e5c6ece8598b6e73c5e12c6acb7b3f1bf713ed9
PRAKRITI <136888456+Nature110625@users.noreply.github.com>
2023-09-24 12:29:26 +0530

    new file

--- 5.Error/exercise12.cpp
+++ 5.Error/exercise12.cpp
@@ -9,48 +9,61 @@ order.*/
 #include "D:\study\std_lib_facilities.h"
 using namespace std;
 
-int main(){
-    //char data type helps to store digits of 4 digit number seperately and evaluation easy
-    vector<char>o_num = {'1', '2', '3', '6'}; //program's 4 digit number
+//checks if program has vector of four different integers or not
+bool check_repeatition(vector<char>entry1){
+    int check_r{0};
 
-    cout<<"Enter the four digit number whose digits are unique\n";
-    bool exit_c=false;
-
-    //loop to to take entry repititively until user wins
-    while(!exit_c){
-        vector<char>e_num;  //player gussed 4 digit number
-        char val{'1'};
-
-        //stores players entry in a vector
-        for(int i=0; i<4; ++i) {
-            cin>>val;
-            e_num.push_back(val);
+    for(int i=0; i<=3; ++i){
+        for(int n=0; n<=3; ++n){
+            if (entry1[i]==entry1[n] && i!=n)
+                ++check_r;  //one is added to check_r for every repeatition
         }
+    }
+    if (check_r==0)
+        return true;    //returns true if there is no repeatition
+    else
+        return false;
+}
 
-        int c_bull{0};
-        int c_cow{0};
+//find and print numbers of bulls and cows, and return number of bulls 
+int check_bulls(vector<char>ans2, vector<char>entry2) {
+    int bull_n{0}, cow_n{0};
+    for(int i=0; i<=3; ++i){
+        for(int n=0; n<=3; ++n){
+            if (entry2[i]==ans2[n] && i==n)
+                ++bull_n;
+            else if (entry2[i]==ans2[n] && i!=n)
+                ++cow_n;
+        }
+    }
+    cout<<bull_n<<" bulls and "<<cow_n<<" cows";
+    return bull_n;
+}
 
-        //compares player guessed 4 digit number with program's 4 digit number
-        for(int i1=0; i1<e_num.size(); ++i1) {      //select element from user guessed number's vector
-            for(int i2=0; i2<o_num.size(); ++i2){       //select element from program's number vector
-                if (e_num[i1]==o_num[i2]){      //checks if numbers matches
-                    if(i1==i2)      ///checks if position of matched numbers matches
-                        ++c_bull;
-                    else
-                        ++c_cow;
-                }
+int main(){
+    cout<<"BULLS AND COWS\n"
+        <<"In this game you have to guess four digit number of four different integers (in the range 0 to 9)\n"
+        <<"You have to discover those numbers by repeated guess.\n"
+        <<"Say number to be guessed is 1234 and you guesses 1359, the response should be '1 bull and 1 cow' "
+        <<"\nbecause you got one digit (1) right and in a right position (a bull) and one digit (3) right but in a wrong position (a cow)\n"
+        <<"You can guess until you gets four bull.\n"
+        <<"\nLet's start the game";
+    vector<char>ans={'1', '2', '3', '4'};   //the that player has to guess
+    bool check=false;
+    while(!check){
+        vector<char>entry;
+        char a, b, c, d;
+        cout<<"\nEnter a four digit number of four different integers:";
+        cin>>a>>b>>c>>d;
+        entry.push_back(a);
+        entry.push_back(b);
+        entry.push_back(c);
+        entry.push_back(d);
+        if(check_repeatition(entry)) {
+            if(check_bulls(ans, entry)==4){
+                check=true; //leads to exit from while loop
             }
         }
-
-        //prints numbers of cows and bulls
-        cout<<c_bull<<" bull and "<<c_cow<<" cow.\n";
-
-        if(c_bull==4){
-            cout<<"You have guessed right!!";
-            exit_c=true;
-        }
-        else
-            cout<<"Try again: ";
     }
-    return 1;
+    return 0;
 }
